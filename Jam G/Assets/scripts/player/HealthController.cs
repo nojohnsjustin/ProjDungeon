5f75ae0db0954adf8ecba1fc0b1e4999a1d1895f
Justin <NojohnsJustin@gmail.com>
2019-11-24 16:50:59 -0500

    UI/Party
    set up basic UI and party system

---
+++ Jam G/Assets/scripts/player/HealthController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthController : MonoBehaviour
+{
+    public Slider HealthbarSlider;
+    public int CurrentHealth;
+    public int MaxHealth;
+    public Text healthText;
+    bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthbarSlider.maxValue = MaxHealth;
+        HealthbarSlider.value = CurrentHealth;
+        healthText.text = CurrentHealth.ToString() + " /" + MaxHealth.ToString();
+
+
+        if (CurrentHealth <= 0)
+        {
+            if (isDead)
+            {
+                return;
+
+            }
+            Dead();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CurrentHealth -= 10;
+        }
+    }
+
+    void Dead()
+    {
+        isDead = true;
+    }
+}
